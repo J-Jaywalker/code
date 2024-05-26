866cf90f374b0b7ace6445e33ff15464424cd899
James Walker <jamesjarodwalker@hotmail.co.uk>
2024-05-26 15:13:26 +0100

    Added clean build task to tasks.json. Portaudio now linked.

--- VowelAnalyser.cpp
+++ VowelAnalyser.cpp
@@ -1,10 +1,27 @@
+// STL includes
 #include <stdlib.h>
 #include <stdio.h>
+#include "./libs/portaudio/include/portaudio.h"
+
+static void checkError(PaError theError)
+{
+    if (theError != paNoError)
+    {
+        // Print resultant port audio error
+        printf("Port Audio Error: %s\n", Pa_GetErrorText(theError));
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main()
 {
-    printf("Hello world!\n");
+    PaError paError = Pa_Initialize();
+    checkError(paError);
+    printf("Resource allocation success, terminating PortAudio test allocation.\n");
+    
+    paError = Pa_Terminate();
+    checkError(paError);
+    printf("Resources successfulyl deallocated.");
 
     return EXIT_SUCCESS;
 }
-
